1a1a8d5be336da3754a08d5b79d62d93425e03c8
Franklin Sarpong <fmsarp@gmail.com>
2022-10-22 08:10:07 -0400

    23rd Commit

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,33 @@
 // have a function to enter the lottery
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainIdHex in contractAddresses ? contractAddresses[chainId][0] : null
+
     // const {runContractFunction: enterRaffle} = useWeb3Contract({
-    //     abi: ,//
-    //     contractAddress: ,//
-    //     functionName: ,//
+    //     abi: abi,//
+    //     contractAddress: raffleAddress, // specify the networkId
+    //     functionName: "enterRaffle",//
     //     params: {},//
     //     msgValue: ,//
     // })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi, //
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee", //
+        params: {}, //
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            // try to read the raffle entrance fee
+        }
+    }, [])
     return <div>Lottery Entrance button</div>
 }
---
+++ constants/index.js
@@ -0,0 +1,8 @@
+const contractAddress = require("./contractAddresses.json")
+
+const abi = require("./abi.json")
+
+module.exports = {
+    abi,
+    contractAddress,
+}
